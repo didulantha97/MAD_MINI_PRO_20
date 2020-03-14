f91585c9e7e6f9d88798dcfe66e99cec50497817
didulantha97 <60958920+didulantha97@users.noreply.github.com>
2020-03-14 21:35:42 +0530

    hi my 2st commit to this project

--- app/src/main/java/com/sliit/project_elephas/activities/Payment.java
+++ app/src/main/java/com/sliit/project_elephas/activities/Payment.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Layout;
 import android.widget.Button;
 
 import com.sliit.project_elephas.R;
@@ -12,14 +13,21 @@ public class Payment extends AppCompatActivity {
 
     Button pay;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
 
+        //Define components
         pay = (Button) findViewById(R.id.save);
 
+        //set button colors
         pay.setBackgroundColor(Color.parseColor("#03A9F4"));
+
+
+
+
 
     }
 }
---
+++ app/src/main/java/com/sliit/project_elephas/activities/Payment_manage.java
@@ -0,0 +1,16 @@
+package com.sliit.project_elephas.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sliit.project_elephas.R;
+
+public class Payment_manage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_manage);
+    }
+}
