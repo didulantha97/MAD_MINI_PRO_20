7fe24ffc15c712466e13d83c425e0ac529cea248
didulantha97 <60958920+didulantha97@users.noreply.github.com>
2020-03-17 11:22:36 +0530

    Hi i completed all my interfaces

--- app/src/main/java/com/sliit/project_elephas/AfterLoginActivityAdminView.java
+++ app/src/main/java/com/sliit/project_elephas/AfterLoginActivityAdminView.java
@@ -5,24 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import com.sliit.project_elephas.activities.*;
 
 public class AfterLoginActivityAdminView extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_after_login_admin_view);
-    }
 
-    public void redirectWithDataToManageInformationForTours(View view){
-        Intent intent = new Intent(this,ManageInformationForToursActivity.class);
-        // EditText editTextForUsername = findViewById(R.id.editText);
-        //   EditText editTextForPassword = findViewById(R.id.editText2);
-        //String username = editTextForUsername.getText().toString();
-        // String password = editTextForPassword.getText().toString();
-        // intent.putExtra("UserName", username);
-        // intent.putExtra("Password", password);
-        startActivity(intent);
 
     }
+
 }
---
+++ app/src/main/java/com/sliit/project_elephas/activities/Activity_man_cost.java
@@ -0,0 +1,16 @@
+package com.sliit.project_elephas.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sliit.project_elephas.R;
+
+public class Activity_man_cost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_cost);
+    }
+}
