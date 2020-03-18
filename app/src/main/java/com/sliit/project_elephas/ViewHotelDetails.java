59abb6817ad54f6d231589d016b43424595238ec
HashinthiDeshni <61499052+HashinthiDeshni@users.noreply.github.com>
2020-03-18 02:54:26 -0700

    there are 10 warnings, should run it again,no errors.

---
+++ app/src/main/java/com/sliit/project_elephas/ViewHotelDetails.java
@@ -0,0 +1,14 @@
+package com.sliit.project_elephas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewHotelDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_hotel_details);
+    }
+}
